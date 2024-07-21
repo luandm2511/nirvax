b429e96cd3ccf8cb2276f76e7d87040f10a03243
luandm2511 <luandm.work@gmail.com>
2024-07-21 18:03:10 +0700

    Update cart

---
+++ BusinessObject/DTOs/CartDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTOs
+{
+    public class CartDTO
+    {
+        public int TotalCount { get; set; }
+        public List<CartOwner> CartOwners { get; set; } = new List<CartOwner>();
+    }
+}
