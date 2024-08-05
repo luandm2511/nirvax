6ef8a2aa4bc851892892813db93b0e3b589d423c
Windows 10 <jlq6902@gmail.com>
2024-08-05 16:07:28 -0500

    chi806

--- DataAccess/DAOs/WarehouseDAO.cs
+++ DataAccess/DAOs/WarehouseDAO.cs
@@ -153,8 +153,8 @@ namespace DataAccess.DAOs
             //  var paginatedResult = result
             var list = await _context.WarehouseDetails
          .Where(i => i.WarehouseId == warehouse.WarehouseId)
-         .Include(i => i.ProductSize.Product)
-         .Include(i => i.ProductSize.Size)
+      //   .Include(i => i.ProductSize.Product)
+     //    .Include(i => i.ProductSize.Size)
          .Skip((page - 1) * pageSize)
          .Take(pageSize)
          .Select(wd => new WarehouseDetailListDTO
