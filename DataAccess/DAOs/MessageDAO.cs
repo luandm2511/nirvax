3615c27e195f019f61b6e46503ecb2c077e36f25
Windows 10 <jlq6902@gmail.com>
2024-08-25 12:54:57 -0500

    chi26-8

--- DataAccess/DAOs/MessageDAO.cs
+++ DataAccess/DAOs/MessageDAO.cs
@@ -43,7 +43,7 @@ namespace DataAccess.DAOs
         {
             List<MessageDTO> list = new List<MessageDTO>();
                 List<Message> getList = await _context.Messages
-                 .Include(i => i)
+                 .Include(i => i.Room)
                     .Where(i => i.RoomId == roomId)
                     .OrderBy(i=> i.Timestamp)
                     .ToListAsync();
